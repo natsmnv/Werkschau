98bdfdfe8a3395f8085eb23b1cee60650f4149c7
Bojidar Belov <bojidarb.ac@gmail.com>
2026-05-26 15:56:40 +0200

    Added projects from first day

--- src/components/Timetable.tsx
+++ src/components/Timetable.tsx
@@ -1,53 +1,94 @@
 import React from "react";
 import "./Timetable.css";
+
 function Timetable() {
   const GenerateNumberOfBlocks = () => {
-    return Math.floor(Math.random() * 3);
+    return Math.floor(Math.random() * 3 + 1);
   };
 
-  const projects = ["Project1", "Project2"];
-  const projectBlocks = Array.from({ length: projects.length }, () => ({
+  let newLineIndex = 0;
+  const projects1 = [
+    {
+      time: "17:00",
+      text: "Opening \n Werkschau \n (Audimax)",
+    },
+    {
+      time: "17:00-21:00",
+      text: "\n Ausstellung Games \n Ausstellung \n Kommunikationsdesign",
+    },
+    {
+      time: "17:30",
+      text: "Performance \n Audio",
+    },
+    {
+      time: "18:00",
+      text: "Screening \n Animation",
+    },
+    {
+      time: "18:30",
+      text: "Performance \n Audio",
+    },
+    {
+      time: "19:00",
+      text: "Screening Buhle + \n Film Highlights",
+    },
+    {
+      time: "20:00",
+      text: "Performance \n Audio",
+    },
+    {
+      time: "20:30",
+      text: "Filmscreening oder \n Audio Performance",
+    },
+  ];
+
+  const projectBlocks = projects1.map(() => ({
     numberLightBlocks: GenerateNumberOfBlocks(),
     numberDarkBlocks: GenerateNumberOfBlocks(),
   }));
 
-  const ChooseDarkLight = projects.map(() =>
-    Math.random() < 0.5 ? "dark-square" : "light-square",
-  );
-
   return (
     <div>
+      <div className="projects1">
         {/* Generate random number of blocks per project */}
-      {projects.map((project, index) => (
-        <div key={index} className="project">
-           
-           {Array.from(
-            { length: projectBlocks[index].numberLightBlocks },
-            (_, i) => (
-              <div key={`l-${i}`} className="light-square" />
-            )
-          )}
+        {projects1.map((project, index) => (
+          <div key={index} className="project">
+            {Array.from(
+              { length: projectBlocks[index].numberLightBlocks },
+              (_, i) => (
+                <div key={`light-${i}`} className="light-square" />
+              ),
+            )}
 
+            {Array.from(
+              { length: projectBlocks[index].numberDarkBlocks },
+              (_, i) => (
+                <div key={`dark-${i}`} className="dark-square" />
+              ),
+            )}
 
-           {Array.from(
-            { length: projectBlocks[index].numberDarkBlocks },
-            (_, i) => (
-              <div key={`l-${i}`} className="dark-square" />
-            )
-          )}
-
+            {/* Project text */}
             <div className="project-text">
-                <p className="hour">17:00</p>
-          <p>Performance Audio</p>
+              <p className="hour">{project.time}</p>
+
+              <p>
+                {project.text.split("\n").map((line, i) => (
+                  <React.Fragment key={i}>
+                    {line}
+                    <br />
+                  </React.Fragment>
+                ))}
+              </p>
             </div>
-          
+          </div>
+        ))}
+        <div>
+          <p className="weekDayInfo">MONTAG</p>
+          <p className="dateInfo">01.JUNI</p>
         </div>
-      ))}
-
-      {/* Date*/}
-
-
+      </div>
     </div>
   );
 }
+
 export default Timetable;
