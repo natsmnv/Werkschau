305676d7f4f90f11ffda4d1a899c784963800582
Bojidar Belov <bojidarb.ac@gmail.com>
2026-05-28 01:55:26 +0200

    Home page bg image bug fix

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,34 @@
-import React, { useState, useEffect, useRef  } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./Home.css";
 import { motion } from "motion/react";
+
 const Square = () => {
   const [revealed, setRevealed] = useState(false);
+  const timeoutRef = useRef<number | null>(null);
+
+  const reveal = () => {
+    setRevealed(true);
+
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    timeoutRef.current = window.setTimeout(() => {
+      setRevealed(false);
+    }, 20000);
+  };
 
   return (
     <motion.div
       className="square"
-      animate={{ backgroundColor: revealed ? "transparent" : "white" }}
-      transition={{ duration: revealed ? 0.2 : 1 }}
-      onHoverStart={() => setRevealed(true)}
-      onHoverEnd={() => setTimeout(() => setRevealed(false), 20000)}
+      animate={{
+        backgroundColor: revealed ? "transparent" : "white",
+      }}
+      transition={{
+        duration: revealed ? 0.2 : 1,
+      }}
+      onHoverStart={reveal}
+      onTouchStart={reveal}
     />
   );
 };
@@ -21,11 +39,10 @@ function Home() {
   const hasRun = useRef(false);
   const [bgImageIndex, setBgImageIndex] = useState(0);
 
-
   const RandomNumber = () => {
-  const imgIndex = Math.floor(Math.random() * 10) + 1;
-  setBgImageIndex(imgIndex);
-};
+    const imgIndex = Math.floor(Math.random() * 10) + 1;
+    setBgImageIndex(imgIndex);
+  };
 
   useEffect(() => {
     if (hasRun.current) return;
@@ -33,7 +50,7 @@ function Home() {
     hasRun.current = true;
     RandomNumber();
   }, []);
-  
+
   return (
     <div className="home">
       <div className="image-blob">
@@ -42,8 +59,14 @@ function Home() {
             <Square key={i} />
           ))}
         </div>
-        <img src={`../../photos/human at core/background${bgImageIndex}.png`} alt="BG1" className="hero-image" />
+
+        <img
+          src={`/photos/human-at-core/background${bgImageIndex}.png`}
+          alt="BG1"
+          className="hero-image"
+        />
       </div>
+
       <div className="information-container">
         <div className="information-left">
           <p>
