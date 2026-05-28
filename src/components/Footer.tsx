d2e9e544bf8a9662caa3de6381fecb8005271d90
Bojidar Belov <bojidarb.ac@gmail.com>
2026-05-28 14:54:52 +0200

    footer v.1

---
+++ src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import "./Home.css";
+
+function Footer() {
+
+    return (
+        <div>
+            <div className="home">
+                <div className="information-right">
+                    <p className="information-right-location">LOCATION</p>
+                    <p>FH Salzburg</p>
+                    <p>Urstein S 1, 5412 Puch</p>
+                </div>
+            </div>
+            <div className="footer-links">
+                <p>instagram</p>
+                <p>areyouai</p>
+                <p>fhsalzburg</p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
