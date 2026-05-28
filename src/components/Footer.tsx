f3ec78eca80a54b04384d564bd2968703b631fdf
Bojidar Belov <bojidarb.ac@gmail.com>
2026-05-29 00:09:59 +0200

    Footer links added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import "./Home.css";
+import { LuSquareArrowOutUpRight } from "react-icons/lu";
 
 function Footer() {
-
     return (
         <div>
             <div className="home">
@@ -11,10 +11,35 @@ function Footer() {
                     <p>Urstein S 1, 5412 Puch</p>
                 </div>
             </div>
+
             <div className="footer-links">
-                <p>instagram</p>
-                <p>areyouai</p>
-                <p>fhsalzburg</p>
+                <a
+                    href="https://www.instagram.com/werkschau.fhsalzburg/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    instagram <LuSquareArrowOutUpRight />
+                </a>
+
+                <a
+                    href="https://www.linkedin.com/company/fhsalzburg-ct/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    linkedin <LuSquareArrowOutUpRight />
+                </a>
+
+                <a
+                    href="https://www.fh-salzburg.ac.at/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    fhsalzburg <LuSquareArrowOutUpRight />
+                </a>
+
+                <a href="/imprint">
+                    imprint <LuSquareArrowOutUpRight />
+                </a>
             </div>
         </div>
     );
