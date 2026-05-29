7040d651e8ee7026707a77cb219e2bcf018cc973
Bojidar Belov <bojidarb.ac@gmail.com>
2026-05-29 06:18:07 +0200

    burger menu added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="Navbar">
       <h1 className="hero-title">WERKSCHAU 2026</h1>
 
-      <nav>
+      {/* Hamburger Button */}
+      <button
+        className="burger"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </button>
+
+      <nav className={menuOpen ? "nav-open" : ""}>
         <NavLink className="nav-items" to="/">
           home
         </NavLink>
@@ -19,14 +29,10 @@ function NavBar() {
         <NavLink className="nav-items" to="/projects">
           projects
         </NavLink>
-        
+
         <NavLink className="nav-items" to="/about">
           about
         </NavLink>
-{/*
-        <NavLink to="/projects" exact>
-          about
-        </NavLink> */}
       </nav>
     </div>
   );
